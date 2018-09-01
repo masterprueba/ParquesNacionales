302577dc152a72c2c7fd93cd1fab88540c929237
Yoimar Angulo <joimar.angulo.com.co>
2018-08-31 21:33:50 -0500

    Internacionalizacion

--- ParquesNacionalesFront/src/main/java/Beans/ParqueBean.java
+++ ParquesNacionalesFront/src/main/java/Beans/ParqueBean.java
@@ -9,36 +9,40 @@ import Entity.ParqueNatural;
 import Services.ParqueService;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.ws.rs.client.Entity;
-
 
 /**
  *
  * @author Usuario
  */
-@ManagedBean(name="beanParque")
+@ManagedBean(name = "beanParque")
 @ApplicationScoped
-public class ParqueBean implements Serializable{
-    
+public class ParqueBean implements Serializable {
+
     private List<ParqueNatural> parques;
     private List<ParqueNatural> filteredParque;
     private String idPark;
     private ParqueNatural parque;
-    
+    private static ResourceBundle bundle;
+
     @ManagedProperty("#{parqueService}")
     private ParqueService service;
-    
+
     @PostConstruct
     public void init() {
         parques = service.createParks();
         parque = new ParqueNatural();
+
     }
 
     public String getIdPark() {
@@ -56,64 +60,69 @@ public class ParqueBean implements Serializable{
     public void setParque(ParqueNatural parque) {
         this.parque = parque;
     }
-    
+
     public List<String> getStates() {
         return service.getstate();
     }
-    
+
     public List<ParqueNatural> getParks() {
         return parques;
     }
-    
+
     public List<ParqueNatural> getFilteredParks() {
         return filteredParque;
     }
-    
+
     public void setFilteredParks(List<ParqueNatural> filteredParks) {
         this.filteredParque = filteredParks;
     }
-    
+
     public void setService(ParqueService service) {
         this.service = service;
     }
-    
-    public int editar(ParqueNatural parque){       
-            Entity payload = parque.toJson();        
-            return service.editar(payload, parque.getId());
-                        
+
+    public int editar(ParqueNatural parque) {
+        Entity payload = parque.toJson();
+        return service.editar(payload, parque.getId());
     }
+
     public void addMessage(String summary, Severity mes) {
-        FacesMessage message = new FacesMessage(mes, summary,  null);
+        FacesMessage message = new FacesMessage(mes, summary, null);
         FacesContext.getCurrentInstance().addMessage(null, message);
-    }       
-    
-    public void delete(ParqueNatural parque) { 
+    }
+
+    public void delete(ParqueNatural parque) {
+        getLocalidad();
         if (parque != null) {
             int estado = service.Eliminar(parque.getId());
             if (estado == 204) {
                 parques.remove(parque);
-                addMessage("Eliminado : "+parque.getName(),FacesMessage.SEVERITY_INFO);
-            }else{
-                addMessage("Error eliminando : "+parque.getName(),FacesMessage.SEVERITY_ERROR);
+                addMessage(bundle.getString("MSG_ELIMINAR") +" : " + parque.getName(), FacesMessage.SEVERITY_INFO);
+            } else {
+                addMessage(bundle.getString("MSG_ERROR_ELIMINAR") +" : " + parque.getName(), FacesMessage.SEVERITY_ERROR);
             }
-        }else{
-            addMessage("Error objecto no cargado en la API",FacesMessage.SEVERITY_ERROR);
+        } else {
+            addMessage("Error API", FacesMessage.SEVERITY_ERROR);
         }
-        
-        
+
     }
-    
-    public void crear(){
-           Entity payload = parque.toJsonCrear();        
-            int estado = service.crear(payload);
-            if (estado == 201) {
-                parque.setStatus("Open");
-                parques.add(parque);
-                addMessage("Creado : "+parque.getName(),FacesMessage.SEVERITY_INFO);
-            }else{
-                addMessage("Error creando : "+parque.getName()+" "+estado,FacesMessage.SEVERITY_ERROR);
-            } 
-        
+
+    public void crear() {
+        getLocalidad();
+        Entity payload = parque.toJsonCrear();
+        int estado = service.crear(payload);
+        if (estado == 201) {
+            parque.setStatus("Open");
+            parques.add(parque);
+            addMessage(bundle.getString("MSG_CREADO") +" : " + parque.getName(), FacesMessage.SEVERITY_INFO);
+        } else {
+            addMessage(bundle.getString("MSG_ERROR_CREANDO") +" : " + parque.getName() + " " + estado, FacesMessage.SEVERITY_ERROR);
+        }
+
     }
-    
+
+    private void getLocalidad() {
+        UIViewRoot vr = FacesContext.getCurrentInstance().getViewRoot();
+        bundle = ResourceBundle.getBundle("co.com.iteria.parques.bundle.ParqueNacional", vr.getLocale());
+    }
 }
--- ParquesNacionalesFront/src/main/java/Services/ParqueInfo.java
+++ ParquesNacionalesFront/src/main/java/Services/ParqueInfo.java
@@ -10,11 +10,13 @@ import Entity.ParqueNatural;
 import javax.faces.event.ActionEvent;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.ResourceBundle;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
@@ -28,19 +30,21 @@ import javax.ws.rs.core.MediaType;
  */
 @ManagedBean
 @ViewScoped
-public class ParqueInfo implements Serializable{
-   private String selectedId;
+public class ParqueInfo implements Serializable {
+
+    private String selectedId;
     private ParqueNatural selectedParque = new ParqueNatural();
-    
+    private static ResourceBundle bundle;
+
     @ManagedProperty("#{beanParque}")
     private ParqueBean parque;
-    
+
     @PostConstruct
     public void init() {
-      FacesContext fc = FacesContext.getCurrentInstance();
-      Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
-      selectedId =  params.get("idpark"); 
-      Client client = ClientBuilder.newClient();
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+        selectedId = params.get("idpark");
+        Client client = ClientBuilder.newClient();
         WebTarget rs = client.target("https://private-f57ba-parques5.apiary-mock.com/parks/{id}");
         JsonObject jsonObj = (JsonObject) rs.resolveTemplate("id", selectedId)
                 .request(MediaType.APPLICATION_JSON)
@@ -55,7 +59,7 @@ public class ParqueInfo implements Serializable{
     public void setParque(ParqueBean parque) {
         this.parque = parque;
     }
-    
+
     public String getSelectedId() {
 
         return selectedId;
@@ -73,19 +77,23 @@ public class ParqueInfo implements Serializable{
         this.selectedParque = selectedParque;
     }
 
-    public void editarPark(ActionEvent actionEvent){ 
-        if(!selectedParque.getId().isEmpty()){
-            if(parque.editar(selectedParque) == 204){
-                parque.addMessage("Supuestamente edito con estado 204", FacesMessage.SEVERITY_INFO);
-            }else{
-                parque.addMessage("error editando...", FacesMessage.SEVERITY_INFO);
+    public void editarPark(ActionEvent actionEvent) {
+        getLocalidad();
+        if (!selectedParque.getId().isEmpty()) {
+            if (parque.editar(selectedParque) == 204) {
+                parque.addMessage(bundle.getString("MSG_EDITADO"), FacesMessage.SEVERITY_INFO);
+            } else {
+                parque.addMessage(bundle.getString("MSG_ERROR_EDITANDO"), FacesMessage.SEVERITY_INFO);
             }
-        }else{
-           parque.addMessage("Erro no cargado en API", FacesMessage.SEVERITY_INFO); 
+        } else {
+            parque.addMessage("Error API", FacesMessage.SEVERITY_INFO);
         }
-        
+
     }
-    
-    
-    
+
+    private void getLocalidad() {
+        UIViewRoot vr = FacesContext.getCurrentInstance().getViewRoot();
+        bundle = ResourceBundle.getBundle("co.com.iteria.parques.bundle.ParqueNacional", vr.getLocale());
+    }
+
 }
